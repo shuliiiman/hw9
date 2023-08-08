a15561f74818475ee940031dc0dfb0c842445a65
Alexander Shulimov <a.shulimov@yandex.ru>
2023-08-08 20:26:46 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-
+        task3();
+        task4();
     }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -12,18 +14,55 @@ public class Main {
         }
         return arr;
     }
-    public static void task1(){
+
+    public static void task1() {
         System.out.println("Задание номер 1");
-            int[] arr = generateRandomArray();
-            int sum=0;
+        int[] arr = generateRandomArray();
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum+=arr[i];
-            break;
+            sum += arr[i];
         }
-        System.out.println("Сумма трат за месяц составила "+sum+" рублей.");
-        }
-    public static void task2(){
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+    }
+
+    public static void task2() {
         System.out.println("Задание номер 2");
+        int[] arr = generateRandomArray();
+        int maxSum = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxSum) {
+                maxSum = arr[i];
+            }
+        }
+        int minSum = arr[arr.length - 1];
+        for (int i = arr.length - 1; i >= arr.length; i--) {
+            if (arr[i] < minSum) {
+                minSum = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minSum + " рублей. Максимальная сумма трат за день составила " + maxSum + " рублей");
+    }
+
+    public static void task3() {
+        System.out.println("Задание номер 3");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        float mediumOfExpensive = sum / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила" + mediumOfExpensive);
+    }
+
+    public static void task4() {
+        System.out.println("Задание номер 4");
+        char[] reverseFullName ={'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        char a = 0;
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            a += reverseFullName[i];
+            System.out.print(a);
+        }
 
     }
+
 }
